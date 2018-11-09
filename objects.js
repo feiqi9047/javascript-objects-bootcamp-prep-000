0fbaa1ec3b62c61e28991520506fdbcc8810bcdd
Fei Qi <feiqi9047@gmail.com>
2018-11-09 22:20:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {
   "CJ":"love me"
 }
@@ -10,4 +11,10 @@ function updatePlaylist (playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName){
   delete playlist[artistName]
   return playlist
+=======
+var playlist = {CJ:"love me"};
+
+function updatePlaylist (playlist, key, value){
+  return object.assign({'Untitled'}, playlist, {[key]:value})
+>>>>>>> 47a16295fbe2feff5d02317fef014979fd6777f2
 }
